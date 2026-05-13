2429bd8e927c3752fc2b712af78689b53b5e06fd
Jaideep Sharma <JAIDEEP13130@GMAIL.COM>
2026-05-13 06:46:42 +0530

    Fixed mongoose connection type error preventing Vercel build

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 declare global {
   var mongoose: {
-    conn: mongoose.Connection | null;
-    promise: Promise<mongoose.Connection> | null;
+    conn: typeof mongoose | null;
+    promise: Promise<typeof mongoose> | null;
   };
 }
