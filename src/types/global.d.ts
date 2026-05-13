4f0dd1681a119b86520e7d0229dc52081227af86
Jaideep Sharma <JAIDEEP13130@GMAIL.COM>
2026-05-13 06:52:28 +0530

    Fixed mongoose strict TypeScript bug causing Vercel builds to fail

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
 declare global {
-  var mongoose: {
-    conn: typeof mongoose | null;
-    promise: Promise<typeof mongoose> | null;
-  };
+  var mongoose: any;
 }
